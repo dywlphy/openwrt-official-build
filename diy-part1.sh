5b396e9455c38efe677722d8f6eb7cfe8c9875d7
dywlphy <87897108+dywlphy@users.noreply.github.com>
2026-05-15 21:26:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,6 +24,7 @@ cat > feeds.conf << 'EOF'
 src-git packages https://github.com/openwrt/packages.git;openwrt-24.10
 src-git luci https://github.com/openwrt/luci.git;openwrt-24.10
 src-git printing https://github.com/dywlphy/openwrt-feed-printing.git;main
+src-git timecontrol https://github.com/sirpdboy/luci-app-timecontrol
 EOF
 
 echo "[2/3] 当前feeds配置:"
